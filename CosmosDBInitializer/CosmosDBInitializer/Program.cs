4209b5d1ec0d8f6aaba6a2284477015edcf772db
cwissmann <mail@c-wissmann.de>
2018-11-21 17:45:20 +0100

    init db initializer

---
+++ CosmosDBInitializer/CosmosDBInitializer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CosmosDBInitializer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
