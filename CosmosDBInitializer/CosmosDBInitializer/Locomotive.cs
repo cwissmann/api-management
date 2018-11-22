b7f1d6def56593ebcf354b026cde2ff30c0f1e21
cwissmann <mail@c-wissmann.de>
2018-11-22 14:11:42 +0100

    add locomotives to db initializer

---
+++ CosmosDBInitializer/CosmosDBInitializer/Locomotive.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace CosmosDBInitializer
+{
+    public class Locomotive
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string Baureihe { get; set; }
+
+        public string Typ { get; set; }
+
+        public int Laenge { get; set; }
+
+        public int Dienstmasse { get; set; }
+
+        public int Vmax { get; set; }
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
--- CosmosDBInitializer/CosmosDBInitializer/Program.cs
+++ CosmosDBInitializer/CosmosDBInitializer/Program.cs
@@ -1,12 +1,81 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
 
 namespace CosmosDBInitializer
 {
     class Program
     {
+        private const string EndpointUri = "https://[API].documents.azure.com:443/";
+        private const string PrimaryKey = "[PRIMARYKEY]";
+
+        private const string dbName = "dojodb";
+        private const string collectionName = "locomotives";
+
+        private DocumentClient client;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Initialize Cosmos DB");
+
+            var locomotives = new List<Locomotive>();
+            locomotives.Add(new Locomotive { Baureihe = "103", Typ = "E", Laenge = 19500, Dienstmasse = 114, Vmax = 200 });
+            locomotives.Add(new Locomotive { Baureihe = "01", Typ = "Dampf", Laenge = 23940, Dienstmasse = 109, Vmax = 120 });
+            locomotives.Add(new Locomotive { Baureihe = "52", Typ = "Dampf", Laenge = 22975, Dienstmasse = 84, Vmax = 80 });
+            locomotives.Add(new Locomotive { Baureihe = "V200", Typ = "Diesel", Laenge = 18470, Dienstmasse = 80, Vmax = 140 });
+            locomotives.Add(new Locomotive { Baureihe = "151", Typ = "E", Laenge = 19490, Dienstmasse = 118, Vmax = 120 });
+
+            try
+            {
+                Program p = new Program();
+                p.AccessCosmosDb().Wait();
+
+                p.client.CreateDatabaseIfNotExistsAsync(new Database { Id = dbName });
+
+                p.client.CreateDocumentCollectionIfNotExistsAsync(UriFactory.CreateDatabaseUri(dbName), new DocumentCollection { Id = collectionName });
+
+                foreach (var locomotive in locomotives)
+                {
+                    p.InsertDocument(dbName, collectionName, locomotive).Wait();
+                    Console.WriteLine($"Insert Baureihe {locomotive.Baureihe}");
+                }
+            }
+            catch (DocumentClientException de)
+            {
+                Exception baseException = de.GetBaseException();
+                Console.WriteLine($"{de.StatusCode} error occured: {de.Message}, Message: {baseException.Message}");
+            }
+            catch (Exception e)
+            {
+                Exception baseException = e.GetBaseException();
+                Console.WriteLine($"Error: {e.Message}, Message: {baseException.Message}");
+            }
+            finally
+            {
+                Console.WriteLine("End of CosmosDB access, press any key to exit.");
+                Console.ReadKey();
+            }
+        }
+
+        private async Task AccessCosmosDb()
+        {
+            this.client = new DocumentClient(new Uri(EndpointUri), PrimaryKey);
+        }
+
+        private async Task InsertDocument(string databaseName, string collectionName, Locomotive locomotive)
+        {
+            try
+            {
+                await this.client.CreateDocumentAsync(UriFactory.CreateDocumentCollectionUri(databaseName, collectionName), locomotive);
+            }
+            catch (DocumentClientException de)
+            {
+                Exception baseException = de.GetBaseException();
+                Console.WriteLine($"Error: {de.Message}, Message: {baseException.Message}");
+            }
+
         }
     }
 }
