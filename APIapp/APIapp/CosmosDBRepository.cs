d45f6ca71e9a4a6c24badb994abb594f05e8be1d
Christian Wißmann <christian.wissmann@saxsys.de>
2018-11-26 12:57:36 +0100

    add db repository and data model

---
+++ APIapp/APIapp/CosmosDBRepository.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Microsoft.Azure.Documents.Client;
+using Microsoft.Azure.Documents.Linq;
+
+namespace APIapp
+{
+    public static class CosmosDBRepository<T> where T : class
+    {
+        private static readonly string Endpoint = "URI";
+        private static readonly string PrimaryKey = "KEY";
+
+        private static readonly string DatabaseId = "DATABASEID";
+        private static readonly string CollectionId = "COLLECTIONID";
+
+        private static DocumentClient client;
+
+        public static void TypeInitializationException()
+        {
+            client = new DocumentClient(new Uri(Endpoint), PrimaryKey);
+        }
+
+        public static async Task<IEnumerable<T>> GetItemsAsync()
+        {
+            IDocumentQuery<T> query = client.CreateDocumentQuery<T>(
+                    UriFactory.CreateDocumentCollectionUri(DatabaseId, CollectionId))
+                .AsDocumentQuery();
+
+            List<T> results = new List<T>();
+
+            while (query.HasMoreResults)
+            {
+                results.AddRange((await query.ExecuteNextAsync<T>()));
+            }
+
+            return results;
+        }
+
+        public static async Task<IEnumerable<T>> GetItemsAsync(Expression<Func<T, bool>> predicate)
+        {
+            IDocumentQuery<T> query = client.CreateDocumentQuery<T>(
+                    UriFactory.CreateDocumentCollectionUri(DatabaseId, CollectionId))
+                .Where(predicate)
+                .AsDocumentQuery();
+
+            List<T> results = new List<T>();
+            while (query.HasMoreResults)
+            {
+                results.AddRange(await query.ExecuteNextAsync<T>());
+            }
+
+            return results;
+        }
+    }
+}
---
+++ APIapp/APIapp/Models/Locomotive.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace APIapp.Models
+{
+    public class Locomotive
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string Baureihe { get; set; }
+
+        public string Typ { get; set; }
+
+        public int Laenge { get; set; }
+
+        public int Dienstmasse { get; set; }
+
+        public int Vmax { get; set; }
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
